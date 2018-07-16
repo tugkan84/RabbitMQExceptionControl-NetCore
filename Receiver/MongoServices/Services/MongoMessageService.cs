0937f18ef94bf82ccf0591b3dd7bc9cb77d96ea4
Birkan Tuğcu <birkantugcu@gmail.com>
2018-07-16 23:53:51 +0300

    Update MongoMessageService.cs

--- Receiver/MongoServices/Services/MongoMessageService.cs
+++ Receiver/MongoServices/Services/MongoMessageService.cs
@@ -23,32 +23,13 @@ namespace Receiver.MongoServices.Services
                     .Find(_ => true).ToListAsync();
         }
 
-        // public MongoMessage GetProduct(ObjectId id)
-        // {
-        //     var res = Query<MongoMessage>.EQ(p => p.Id, id);
-        //     return _db.GetCollection<MongoMessage>("Message").FindOne(res);
-        // }
-
         public MongoMessage Create(MongoMessage p)
         {
             _db.GetCollection<MongoMessage>("Message").InsertOneAsync(p);
             return p;
         }
 
-        // public void Update(ObjectId id, MongoMessage p)
-        // {
-        //     p.Id = id;
-        //     var res = Query<MongoMessage>.EQ(pd => pd.Id, id);
-        //     var operation = Update<MongoMessage>.Replace(p);
-        //     _db.GetCollection<MongoMessage>("Message").Update(res, operation);
-        // }
         
-        // public void Remove(ObjectId id)
-        // {
-        //     var res = Query<MongoMessage>.EQ(e => e.Id, id);
-        //     var operation = _db.GetCollection<MongoMessage>("Message").Remove(res);
-        // }
-
         public void RemoveAll(){
             _db.GetCollection<MongoMessage>("Message").DeleteManyAsync(new BsonDocument());
         }
